e798475f84bf1ae05eb3ab7d0c268a105bddd2b8
Chris Geihsler <chris@geihsler.net>
2012-08-25 12:08:03 -0400

    refactoring var integer parsing

--- lib/pure-sqlite/structures/variable_length_integer.rb
+++ lib/pure-sqlite/structures/variable_length_integer.rb
@@ -22,36 +22,34 @@ module PureSQLite
       LAST_SEVEN_BITS_MASK    = 0b01111111
 
       def parse(stream)
-        counter = 0
-        value   = 0
+        value = 0
 
-        loop do
+        (1..9).each do |counter|
           byte = stream.readbyte()
-          counter += 1
 
           is_ninth_byte = (counter == 9)
-          byte_starts_with_zero = (byte & IS_FIRST_BIT_ZERO_MASK == 0)
 
           usable_size = is_ninth_byte ? 8 : 7
-
-          if(usable_size == 7)
-            byte &= LAST_SEVEN_BITS_MASK
-          end
-
           value = value << usable_size
-          value += byte
+          value += usable_value(usable_size, byte)
 
-          if(is_ninth_byte || byte_starts_with_zero)
-            break
+          if(is_ninth_byte || starts_with_zero?(byte))
+            return {
+                length: counter,
+                value: Conversions.twos_complement(value)
+            }
           end
-
         end
-
-        {
-            length: counter,
-            value: Conversions.twos_complement(value)
-        }
       end
+
+      def starts_with_zero?(byte)
+        byte & IS_FIRST_BIT_ZERO_MASK == 0
+      end
+
+      def usable_value(usable_size, byte)
+        usable_size == 7 ? byte & LAST_SEVEN_BITS_MASK : byte
+      end
+
     end
   end
 end
